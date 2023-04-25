7821b4eb30877a08c9d95c209330c67c6e36e86d
nBfour <nguyennhathao11c2@gmail.com>
2023-04-25 19:14:16 +0700

    up project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from 'react';
+import Header from "./Header&Footer/Header";
+import Footer from "./Header&Footer/Footer";
+import HeaderandFooter from "./Header&Footer/HeaderandFooter";
+import Search from "./SearchPage/Search";
+import Home from "./HomePage/Home";
+// import { BrowserRoute, Routes, Route } from 'react-router-dom';
+import UpdateProfile from "./Account/UpdateProfile";
+import AdminPage from "./AdminPage/AdminPage";
+import zBookDetail from "./zDraft/BookDetail";
+import BookDetail from "./BookDetail/BookDetail";
+import BookView from "./BookDetail/BookView";
+import OrderBook from "./OrderManagement/OrderBook";
+import CartandLoved from "./CartandLoved";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return ( 
+    <div className = "App" > 
+      {/* <BrowserRoute>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRoute> */}
+
+      {/* <HeaderandFooter/> */}
+      {/* <Search /> */}
+      {/* <BookView />  */}
+      <AdminPage /> 
+      </div>
   );
 }
 
